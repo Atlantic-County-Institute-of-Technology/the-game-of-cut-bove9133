908089884674588f02201aae43eba762e97fdd0c
bove9133 <bove9133@student.acitech.org>
2025-11-03 11:12:11 -0500

    Update The_Game_of_Cut
    I forgor to commit this last week

--- src/src/The_Game_of_Cut.java
+++ src/src/The_Game_of_Cut.java
@@ -1,2 +1,11 @@
 public class The_Game_of_Cut {
 }
+
+private Card[] deck = new Card[52]
+        for () {
+            for () {
+                deck[i] = new Card(face, suit)
+                }
+                }
+
+        
