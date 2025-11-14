203a843973d88c2f384ba9711ab1900018bcfb82
bove9133 <bove9133@student.acitech.org>
2025-11-14 12:56:32 -0500

    Update deck.java
    Im almost done, I need to add comments

--- src/src/deck.java
+++ src/src/deck.java
@@ -1,2 +1,24 @@
+import java.util.Random;
+
 public class deck {
+    Card[] deck = new Card[52];
+}
+
+public void newDeck() {
+    int index = 0;
+    for(int x = 1; x < 14; x++){
+        for(int y = 1; y < 5; y++){
+            Card card = new Card(x,y)
+            deck[index] = card; index++;
+        }
+    }
+}
+
+public void shuffleDeck() {
+    Random rand = new Random();
+    Card temp;
+    for(int x = 0; x < 52; x++){
+        for(int y = 0; y < 5; y++)
+        }
+    }
 }
