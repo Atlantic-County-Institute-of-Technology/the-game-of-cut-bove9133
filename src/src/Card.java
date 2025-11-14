a79f07c05af20aa73ae7f020fa391b6c56197462
bove9133 <bove9133@student.acitech.org>
2025-11-14 11:05:24 -0500

    Update Game of cut
    Almost finished, just gotta lock in

---
+++ src/src/Card.java
@@ -0,0 +1,20 @@
+public class Card {
+}
+
+private Card[] deck = new Card[52]
+        for () {
+            for () {
+                deck[i] = new Card(face, suit)
+                }
+                }
+
+public Card(int face, int suit) {
+    this.face = face;
+    this.suit = suit;
+}
+
+public int getFace() {return face;}
+
+public int getSuit() {return suit;}
+
+public String toString() { return faces[face] + " of " + suits[suit];}
